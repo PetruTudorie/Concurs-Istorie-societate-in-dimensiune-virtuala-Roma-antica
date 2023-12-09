dd84eedaa0fdbcf34fe20a10bb44ec5a8ce8116b
Petru.Tudorie <112642060+PetruTudorie@users.noreply.github.com>
2023-12-09 20:11:19 +0200

    Add files via upload

---
+++ Proiect istorie/indext.js
@@ -0,0 +1,22 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_u0jxauj";
+  const templateID = "template_rocmlde";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Mesajul tău a fost trimis!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
